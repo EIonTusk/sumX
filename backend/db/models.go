7412b0c29857248db1d2d366fe6af31b83688f2f
EIonTusk <eiontusk@github.com>
2025-08-13 21:26:20 +0200

    make it prettier

--- backend/db/models.go
+++ backend/db/models.go
@@ -7,30 +7,30 @@ import (
 )
 
 type XUser struct {
-	bun.BaseModel `bun:"table:xusers,alias:xu"`
+	bun.BaseModel 						`bun:"table:xusers,alias:xu"`
 
-    ID        int64     `bun:",pk"`
-    UserName  string    `bun:",notnull"`
+    ID        			int64     		`bun:",pk"`
+    UserName  			string    		`bun:",notnull"`
 }
 
 type SummaryData struct {
-	Heading string `json:"heading"`
-	Text    string `json:"text"`
+	Heading 			string 			`json:"heading"`
+	Text    			string 			`json:"text"`
 }
 
 type Summary struct {
-    bun.BaseModel `bun:"table:summaries,alias:s"`
+    bun.BaseModel 						`bun:"table:summaries,alias:s"`
 
-    ID        int64     `bun:",pk,autoincrement"`
-    UserID   int64     `bun:",notnull"` // FK to users.id
-    XUserID   int64     `bun:",notnull"` // FK to users.id
-    From      time.Time `bun:",nullzero"`
-    To        time.Time `bun:",nullzero"`
-    Limit     int16     `bun:",nullzero"`
-    RetweetsIncluded     bool     `bun:",nullzero"`
-	Summary 	[]SummaryData `bun:",nullzero"`
-	Tweets []string	`bun:",nullzero"`	
-    CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+    ID        			int64     		`bun:",pk,autoincrement"`
+    UserID   			int64     		`bun:",notnull"`
+    XUserID   			int64     		`bun:",notnull"`
+    From      			time.Time 		`bun:",nullzero"`
+    To        			time.Time 		`bun:",nullzero"`
+    Limit     			int16     		`bun:",nullzero"`
+    RetweetsIncluded    bool     		`bun:",nullzero"`
+	Summary 			[]SummaryData 	`bun:",nullzero"`
+	Tweets 				[]string		`bun:",nullzero"`	
+    CreatedAt 			time.Time 		`bun:",nullzero,notnull,default:current_timestamp"`
 
-    XUser     *XUser    `bun:"rel:belongs-to,join:x_user_id=id"`
+    XUser     			*XUser    		`bun:"rel:belongs-to,join:x_user_id=id"`
 }
